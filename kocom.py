36aa4f6331a78504686be36e22098e347da91d44
koko317 <149462920+koko317@users.noreply.github.com>
2024-08-06 16:47:09 +0900

    Update kocom.py

--- kocom.py
+++ kocom.py
@@ -9,6 +9,7 @@
  apt-get install mosquitto
  python3 -m pip install pyserial
  python3 -m pip install paho-mqtt
+ python3 -m pip install typing_extensions
 '''
 import os
 import time
@@ -23,7 +24,7 @@ import configparser
 
 
 # define -------------------------------
-SW_VERSION = '2023.08.012'
+SW_VERSION = '2024.08.06'
 CONFIG_FILE = 'kocom.conf'
 BUF_SIZE = 100
 
@@ -51,7 +52,8 @@ room_h_dic = {'livingroom':'00', 'myhome':'00', 'room1':'01', 'room2':'02', 'roo
 # mqtt functions ----------------------------
 
 def init_mqttc():
-    mqttc = mqtt.Client()
+    # mqttc = mqtt.Client() # 삭제
+    mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1) # 추가
     mqttc.on_message = mqtt_on_message
     mqttc.on_subscribe = mqtt_on_subscribe
     mqttc.on_connect = mqtt_on_connect
